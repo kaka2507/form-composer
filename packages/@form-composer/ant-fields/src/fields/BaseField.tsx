02682fbf222848798c83fd6316800936cc95bff0
Bao Doan N <doanngocbao@gmail.com>
2020-09-30 11:53:34 +0700

    Fix: meta.error can be object without root cause now. Try to quick fix

--- packages/@form-composer/ant-fields/src/fields/BaseField.tsx
+++ packages/@form-composer/ant-fields/src/fields/BaseField.tsx
@@ -10,12 +10,18 @@ export interface BaseFieldProps extends FieldRenderProps<any, HTMLElement> {
     help?: string | React.ReactNode
 }
 
+const getError = (error: string | object) => {
+    if (!error) return error
+    if (typeof error === 'string') return error;
+    return Object.values(error).join('')
+}
+
 export const BaseField = ({field, meta, help, itemLayout, children}: BaseFieldProps) => {
     const hasError = meta.touched && meta.error;
     return (
         <>
             {hasError && (
-                <AntForm.Item key={field.name} {...itemLayout} hasFeedback label={field.label} help={meta.error}
+                <AntForm.Item key={field.name} {...itemLayout} hasFeedback label={field.label} help={getError(meta.error)}
                               validateStatus="error">
                     {children}
                 </AntForm.Item>
