c4ca6c15d7c44f23bfb7e9404aed0bb38f6296f6
Bao Doan N <doanngocbao@gmail.com>
2020-10-07 10:28:06 +0700

    Upd: override default margin when noHelp or noLabel

--- packages/@form-composer/ant-fields/src/fields/BaseField.tsx
+++ packages/@form-composer/ant-fields/src/fields/BaseField.tsx
@@ -19,11 +19,15 @@ const getError = (error: string | object) => {
 export const BaseField = ({field, meta, help, itemLayout, children}: BaseFieldProps) => {
     const hasError = meta.touched && meta.error;
     const finalHelp = help? help: (field.description? field.description : undefined)
+    let styles = {}
+    if (field.noHelp) styles['marginBottom'] = '8px'
+    if (field.noLabel) styles['marginTop'] = '8px'
     return (
         <AntForm.Item
             key={field.name}
             label={field.noLabel? undefined : field.label}
             help={field.noHelp? undefined : hasError? getError(meta.error): finalHelp}
+            style={styles}
             validateStatus={hasError? "error": "validating"}
             {...itemLayout}>
             {children}
