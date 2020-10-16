e22e83170b9159e98b16c11d329eca928ed508c6
Bao Doan N <doanngocbao@gmail.com>
2020-10-16 10:39:34 +0700

    Upd: adjust tags after re-implement BaseForm

--- packages/@form-composer/ant-fields/src/fields/TagsFieldComponent.tsx
+++ packages/@form-composer/ant-fields/src/fields/TagsFieldComponent.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import {parseText} from "./format";
 import {Field, FieldComponent, Form} from "@form-composer/core";
-import {Input, Tag, Typography} from 'antd';
+import {Input, Tag} from 'antd';
 import {BaseField, BaseFieldProps} from "./BaseField";
 
-const {Text} = Typography;
 
 interface TagsFieldProps extends BaseFieldProps {
     field: Field & { placeholder: string }
@@ -30,27 +29,8 @@ const TagsField = ({form, field, input, ...rest}: TagsFieldProps) => {
         form.mutators.remove(field.name, index)
     }, [form, field])
     const items = input.value || []
-    const help = (
-        <div>
-            <div>
-                <div>
-                    <Text type="secondary">{field.description}</Text>
-                </div>
-                <div>
-                     <span style={{display: 'flex', flexWrap: 'wrap', margin: '4px 0'}}>
-                        {items && items.map((tag: string, index: number) => (
-                            <Tag key={tag} closable onClose={() => removeTag(index)}>{tag}</Tag>
-                        ))}
-                         {!items && (
-                             <span>{field.description}</span>
-                         )}
-                    </span>
-                </div>
-            </div>
-        </div>
-    )
     return (
-        <BaseField field={field} form={form} input={input} help={help} {...rest}>
+        <BaseField field={field} form={form} input={input} {...rest}>
             <Input
                 key={field.name}
                 name={field.name}
@@ -65,6 +45,16 @@ const TagsField = ({form, field, input, ...rest}: TagsFieldProps) => {
                     }
                 }}
             />
+            <div>
+                     <span style={{display: 'flex', flexWrap: 'wrap', margin: '4px 0'}}>
+                        {items && items.map((tag: string, index: number) => (
+                            <Tag key={tag} closable onClose={() => removeTag(index)}>{tag}</Tag>
+                        ))}
+                         {!items && (
+                             <span>{field.description}</span>
+                         )}
+                    </span>
+            </div>
         </BaseField>
     )
 }
