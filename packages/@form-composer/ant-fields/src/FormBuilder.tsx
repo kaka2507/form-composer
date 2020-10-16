25968300caaffa4961041be0a7678ea6e12f72a9
Bao Doan N <doanngocbao@gmail.com>
2020-10-16 11:49:45 +0700

    Upd: support to define render fields on FormBuilder.tsx

--- packages/@form-composer/ant-fields/src/FormBuilder.tsx
+++ packages/@form-composer/ant-fields/src/FormBuilder.tsx
@@ -1,19 +1,21 @@
 import React, {FC} from 'react'
-import {FieldsBuilder, Form} from '@form-composer/core'
+import {FieldsBuilder, Form, Field} from '@form-composer/core'
 import {Form as FinalForm} from 'react-final-form'
 import "antd/dist/antd.css";
 
 export interface FormBuilderProps {
     form: Form,
+    fields?: Field[]
 }
 
 const FF: any = FinalForm
 
-export const FormBuilder: FC<FormBuilderProps> = ({form}) => {
+export const FormBuilder: FC<FormBuilderProps> = ({form, fields}) => {
+    const renderFields = fields? fields : form.fields
     return (
         <FF form={form.finalForm}>
             {() => (
-                <FieldsBuilder form={form} fields={form.fields}/>
+                <FieldsBuilder form={form} fields={renderFields}/>
             )}
         </FF>
     )
