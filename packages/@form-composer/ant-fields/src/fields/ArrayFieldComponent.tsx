83797e9aaa8fdd5d19911d1524bfb525b6bc2fc7
Bao Doan N <doanngocbao@gmail.com>
2020-10-28 18:06:45 +0700

    Upd: support re-order array items

--- packages/@form-composer/ant-fields/src/fields/ArrayFieldComponent.tsx
+++ packages/@form-composer/ant-fields/src/fields/ArrayFieldComponent.tsx
@@ -3,10 +3,16 @@ import {Button, Collapse, Row, Tooltip} from 'antd';
 import {parseText} from "./format";
 import {Field, FieldBuilder, FieldComponent, Form, useFormComposer} from "@form-composer/core";
 import {BaseField, BaseFieldProps} from "./BaseField";
-import {CloseCircleFilled, InfoCircleOutlined, PlusCircleOutlined} from '@ant-design/icons'
+import {
+    CloseCircleFilled,
+    DownCircleOutlined,
+    InfoCircleOutlined,
+    PlusCircleOutlined,
+    RetweetOutlined,
+} from '@ant-design/icons'
 import {blue, red} from "@ant-design/colors";
 
-const { Panel } = Collapse
+const {Panel} = Collapse
 
 type DefaultItem = any | (() => any)
 
@@ -35,6 +41,12 @@ const ArrayField = ({form, field, input, ...rest}: ArrayFieldProps) => {
         form.mutators.remove(field.name, index)
     }, [form, field])
 
+    const items = input && input.value ? input.value : []
+    const moveDown = React.useCallback((index) => {
+        const dest = index === items.length - 1 ? 0 : index + 1
+        form.mutators.swap(field.name, index, dest)
+    }, [form, field, items.length])
+
     // Mark object field as touched when user click to expand
     const onExpand = React.useCallback(() => {
         const state = form.finalForm.getFieldState(field.name)
@@ -43,7 +55,6 @@ const ArrayField = ({form, field, input, ...rest}: ArrayFieldProps) => {
         }
     }, [form, field])
 
-    const items = input.value || []
     const fields: any[] = React.useMemo(() => {
         return items.map((item, index) => ({
             ...field.child,
@@ -56,7 +67,7 @@ const ArrayField = ({form, field, input, ...rest}: ArrayFieldProps) => {
     const header = (
         <div style={{width: '100%', display: 'flex', justifyContent: 'flex-end'}}>
             <Tooltip placement="top" title="Click to expand">
-                <InfoCircleOutlined />
+                <InfoCircleOutlined/>
             </Tooltip>
         </div>
     )
@@ -77,13 +88,24 @@ const ArrayField = ({form, field, input, ...rest}: ArrayFieldProps) => {
                     {fields.map((field, index) => (
                         <Row key={field.name} align="middle">
                             <div style={{position: 'relative', width: '100%'}}>
-                                <div style={{position: 'absolute', top: '-12px', right: '-20px', zIndex: 100}}>
+                                <div style={{position: 'absolute', top: '-12px', right: '-20px', zIndex: 200}}>
                                     <Button
                                         type="text" size="large"
                                         onClick={() => removeItem(index)}
                                         icon={<CloseCircleFilled style={{color: red[5], fontSize: '20px'}}/>}
                                     />
                                 </div>
+                                {items.length > 1 && (
+                                    <div style={{position: 'absolute', height: '100%', right: '-20px', zIndex: 100}}>
+                                        <Row align="middle" style={{height: '100%'}}>
+                                            <Button
+                                                type="text" size="large"
+                                                onClick={() => moveDown(index)}
+                                                icon={index != 0 && index === items.length - 1 ? <RetweetOutlined /> : <DownCircleOutlined />}
+                                            />
+                                        </Row>
+                                    </div>
+                                )}
                                 <FieldBuilder formComposer={formComposer} form={form} field={field}/>
                             </div>
                         </Row>
@@ -94,7 +116,7 @@ const ArrayField = ({form, field, input, ...rest}: ArrayFieldProps) => {
                                 type="text"
                                 size="large"
                                 onClick={() => addItem()}
-                                icon={<PlusCircleOutlined style={{color: blue[5], fontSize: '20px'}} />}
+                                icon={<PlusCircleOutlined style={{color: blue[5], fontSize: '20px'}}/>}
                             />
                         </Tooltip>
                     </Row>
